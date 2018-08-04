4c5c563404e2b2ba1724170fa47b3a4150bb78ea
Zhen Du <jenkin.du@gmail.com>
2018-08-03 23:19:08 -0400

    ex3

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -7,6 +7,8 @@ function p = predict(Theta1, Theta2, X)
 m = size(X, 1);
 num_labels = size(Theta2, 1);
 
+% Add ones to the X data matrix
+X = [ones(m, 1) X];
 % You need to return the following variables correctly 
 p = zeros(size(X, 1), 1);
 
@@ -20,10 +22,14 @@ p = zeros(size(X, 1), 1);
 %       information see 'help max'. If your examples are in rows, then, you
 %       can use max(A, [], 2) to obtain the max for each row.
 %
-
-
-
-
+predict1 = X * Theta1';
+g1 = sigmoid(predict1);
+m = size(g1, 1);
+% Add ones to the X data matrix
+g1 = [ones(m, 1) g1];
+predict2 = g1 * Theta2';
+g2 = sigmoid(predict2);
+[q,p] = max(g2, [], 2);
 
 
 
--- machine-learning-ex3/ex3/predictOneVsAll.m
+++ machine-learning-ex3/ex3/predictOneVsAll.m
@@ -29,11 +29,8 @@ X = [ones(m, 1) X];
 %       are in rows, then, you can use max(A, [], 2) to obtain the max 
 %       for each row.
 %       
-
-
-
-
-
+predict = X * all_theta';
+[q,p] = max(predict, [], 2);
 
 
 % =========================================================================
