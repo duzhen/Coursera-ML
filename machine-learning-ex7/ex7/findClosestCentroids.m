5a713e85c7c0ecb223ed0c6a3315f34ea9eac9c5
Zhen Du <jenkin.du@gmail.com>
2018-08-06 23:24:24 -0400

    ex7

--- machine-learning-ex7/ex7/findClosestCentroids.m
+++ machine-learning-ex7/ex7/findClosestCentroids.m
@@ -21,7 +21,17 @@ idx = zeros(size(X,1), 1);
 % Note: You can use a for-loop over the examples to compute this.
 %
 
-
+for i=1:size(X,1)
+    d = realmax;
+    for j = 1:K
+        dTmp = (X(i,:)-centroids(j,:))*(X(i,:)-centroids(j,:))';
+        if dTmp < d
+            d = dTmp;
+            idx(i) = j;
+        end;
+    end;
+end;
+    
 
 
 
